0c020d490093c156ff485ecfc356dbbac0f21653
Vrishank Sharma <sharma08462@gmail.com>
2024-07-30 14:45:43 +0530

    bug fix

--- src/components/ui/glare-card.tsx
+++ src/components/ui/glare-card.tsx
@@ -67,7 +67,7 @@ export const GlareCard = ({
     return (
         <div
             style={containerStyle}
-            className="relative isolate [contain:layout_style] [perspective:600px] transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] will-change-transform w-[320px] [aspect-ratio:17/21]"
+            className="relative isolate [contain:layout_style] [perspective:600px] transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] will-change-transform w-auto [aspect-ratio:17/21]"
             ref={refElement}
             onPointerMove={(event) => {
                 const rotateFactor = 0.4;
