898e06515ad9e44b52ecb8ea659dfccc7a213d76
Kumar Harsh <kharsh560@gmail.com>
2024-08-02 14:25:04 +0530

    fixed the bug of 1st row 3rd person looking small.
    fixed the bug of 1st row 3rd person looking small.
    And made rest of the persons in mobile version appear in one single column.

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -9,17 +9,18 @@ export default function Team() {
             OUR TEAM
           </h1>
         </div>
+        {/* First row of 3 pics */}
         <div className=' flex justify-center sm:gap-20 gap-10 p-4 sm:mt-10 mt-2 '>
           {people1.map((eachPerson) => (
             <div className='font-medium'> 
-              <div className='flex justify-center overflow-hidden sm:rounded-2xl rounded-full'>
+              <div className='flex justify-center overflow-hidden sm:rounded-2xl rounded-xl'>
                   {/* <img className=' sm:h-[300px] h-[130px]  sm:hover:scale-[1.2] transition-transform duration-500' src={eachPerson.image_source} /> */}
                   <Image 
                   alt='TeamMemberImage' 
                   layout="responsive" 
                   width={16} 
                   height={9} 
-                  className='sm:h-[300px] sm:w-[200px] h-[130px] sm:hover:scale-[1.2] transition-transform duration-500' 
+                  className='sm:min-h-[300px] sm:w-[200px] min-h-[130px]  sm:hover:scale-[1.2] transition-transform duration-500' 
                   src="https://res.cloudinary.com/dx5dlghqe/image/upload/v1722053621/gwlfy7f9oi1hcbdlczki.jpg" />
               </div>
               <div className='text-xs sm:text-xl text-center p-4'>
@@ -37,18 +38,22 @@ export default function Team() {
             </div>
           ))}
         </div>
-        <div className='justify-items-center sm:gap-20 gap-10 p-4 m-2 sm:mt-20 mt-5 grid grid-cols-4'>
+        {/* Second row of multiple pics */}
+        {/* Earlier, the div below had this class:- {className='justify-items-center sm:gap-20 gap-10 p-4 m-2 sm:mt-20 mt-5 grid grid-cols-4'} */}
+        <div className='justify-items-center sm:gap-20 gap-10 p-4 m-2 sm:mt-20 mt-5 sm:grid sm:grid-cols-4 grid-cols-1'>
           {people2.map((eachPerson) => (
             <div className='font-medium'> 
-              <div className='flex justify-center overflow-hidden sm:rounded-2xl rounded-full'>
+              <div className='flex justify-center overflow-hidden sm:rounded-2xl rounded-xl '>
                 {/* <img className=' sm:h-[300px] h-[130px] sm:hover:scale-[1.2] transition-transform duration-500' src={eachPerson.image_source} /> */}
-                <Image 
-                alt='TeamMemberImage' 
-                layout="responsive" 
-                width={16} 
-                height={9} 
-                className='sm:h-[300px] sm:w-[200px] h-[130px]  sm:hover:scale-[1.2] transition-transform duration-500' 
-                src="https://res.cloudinary.com/dx5dlghqe/image/upload/v1722053621/gwlfy7f9oi1hcbdlczki.jpg" />
+                <div className='sm:h-fit sm:w-[150px] h-fit w-[80px]'>
+                  <Image 
+                  alt='TeamMemberImage' 
+                  layout="responsive" 
+                  width={16} 
+                  height={9} 
+                  className='sm:hover:scale-[1.2] transition-transform duration-500' 
+                  src="https://res.cloudinary.com/dx5dlghqe/image/upload/v1722053621/gwlfy7f9oi1hcbdlczki.jpg" />
+                </div>
               </div>
               <div className='text-xs sm:text-xl  text-center p-4'>
                 <h2 
@@ -66,15 +71,15 @@ export default function Team() {
           ))}
         </div>
         <div className="ml-8 mr-8 border-t-2 border-dashed border-white-600 pt-8"></div>
-        <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-3 justify-items-center">
           {executivesData.map((dept, index) => (
             <div key={dept.name} className="p-4">
               <h2 className="text-xl text-center font-bold mb-2">{dept.name}</h2>
               <ul className={`
                 text-center
-                ${index % 3 === 0 ? 'md:text-left' : ''}
-                ${index % 3 === 1 ? 'md:text-center' : ''}
-                ${index % 3 === 2 ? 'md:text-right' : ''}
+                ${index % 3 === 0 ? 'sm:text-left' : ''}
+                ${index % 3 === 1 ? 'sm:text-center' : ''}
+                ${index % 3 === 2 ? 'sm:text-right' : ''}
               `}>
                 {dept.people.map((person) => (
                   <li className='text-white' key={person}>{person}</li>
