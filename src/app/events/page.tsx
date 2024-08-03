e3c7e4f7d434835ae881804c13aa376ae9aea130
joyal-jij0 <joyal2405@gmail.com>
2024-08-03 23:12:49 +0530

    refactor: Update HeroParallaxDemo component with new product data

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,66 +1,68 @@
 "use client";
-import React from "react";
 import { HeroParallax } from "@/components/ui/hero-parallax";
 
 export default function HeroParallaxDemo() {
+
+  const products = [
+    {
+      title: "Moonbeam",
+      content: " Sed ipsum ipsum, rutrum eget feugiat vitae, ornare nec sem. Duis nec diam quam. Nulla nec erat elementum felis sodales lacinia. Nulla facilisi. Donec tempor erat eu felis fringilla, eleifend scelerisque velit venenatis. Proin eget nunc a metus auctor lobortis at cursus tellus. Cras ac consectetur orci. Suspendisse posuere velit lacus, blandit tempor leo commodo eu. Ut risus eros, aliquet a laoreet ac, bibendum vitae mi. Ut hendrerit, elit id elementum aliquet, lacus mauris ornare nisi, non faucibus nunc urna id dui. Suspendisse potenti. Nunc blandit lacus et nisl eleifend rutrum. Maecenas sodales, lacus vel tincidunt hendrerit, lacus tellus lacinia nisl, in suscipit orci mi et dolor.   ",
+      description: " Sed ipsum ipsum, rutrum eget feugiat vitae, ornare nec sem. Duis nec diam quam. Nulla nec erat elementum felis sodales lacinia. Nulla facilisi. Donec tempor erat eu felis fringilla, eleifend scelerisque velit venenatis. Proin eget nunc a metus auctor lobortis at cursus tellus. Cras ac consectetur orci. Suspendisse posuere velit lacus, blandit tempor leo commodo eu. Ut risus eros, aliquet a laoreet ac, bibendum vitae mi. Ut hendrerit, elit id elementum aliquet, lacus mauris ornare nisi, non faucibus nunc urna id dui. Suspendisse potenti. Nunc blandit lacus et nisl eleifend rutrum. Maecenas sodales, lacus vel tincidunt hendrerit, lacus tellus lacinia nisl, in suscipit orci mi et dolor.    ",
+      thumbnail:
+        "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
+    },
+    {
+      title: "Moonbeam",
+      content: "content is here",
+      description: "descrirption is here",
+      thumbnail:
+        "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
+    },
+    {
+      title: "Moonbeam",
+      content: "content is here",
+      description: "descrirption is here",
+      thumbnail:
+        "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
+    },
+    {
+      title: "Moonbeam",
+      content: "content is here",
+      description: "descrirption is here",
+      thumbnail:
+        "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
+    },
+    {
+      title: "Moonbeam",
+      content: "content is here",
+      description: "descrirption is here",
+      thumbnail:
+        "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
+    },
+    {
+      title: "Moonbeam",
+      content: "content is here",
+      description: "descrirption is here",
+      thumbnail:
+        "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
+    },
+    {
+      title: "Moonbeam",
+      content: "content is here",
+      description: "descrirption is here",
+      thumbnail:
+        "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
+    },
+    {
+      title: "Moonbeam",
+      content: "content is here",
+      description: "descrirption is here",
+      thumbnail:
+        "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
+    },
+    
+  ];
+
   return <HeroParallax products={products} />;
 }
-export const products = [
-  {
-    title: "Moonbeam",
-    content: " Sed ipsum ipsum, rutrum eget feugiat vitae, ornare nec sem. Duis nec diam quam. Nulla nec erat elementum felis sodales lacinia. Nulla facilisi. Donec tempor erat eu felis fringilla, eleifend scelerisque velit venenatis. Proin eget nunc a metus auctor lobortis at cursus tellus. Cras ac consectetur orci. Suspendisse posuere velit lacus, blandit tempor leo commodo eu. Ut risus eros, aliquet a laoreet ac, bibendum vitae mi. Ut hendrerit, elit id elementum aliquet, lacus mauris ornare nisi, non faucibus nunc urna id dui. Suspendisse potenti. Nunc blandit lacus et nisl eleifend rutrum. Maecenas sodales, lacus vel tincidunt hendrerit, lacus tellus lacinia nisl, in suscipit orci mi et dolor.   ",
-    description: " Sed ipsum ipsum, rutrum eget feugiat vitae, ornare nec sem. Duis nec diam quam. Nulla nec erat elementum felis sodales lacinia. Nulla facilisi. Donec tempor erat eu felis fringilla, eleifend scelerisque velit venenatis. Proin eget nunc a metus auctor lobortis at cursus tellus. Cras ac consectetur orci. Suspendisse posuere velit lacus, blandit tempor leo commodo eu. Ut risus eros, aliquet a laoreet ac, bibendum vitae mi. Ut hendrerit, elit id elementum aliquet, lacus mauris ornare nisi, non faucibus nunc urna id dui. Suspendisse potenti. Nunc blandit lacus et nisl eleifend rutrum. Maecenas sodales, lacus vel tincidunt hendrerit, lacus tellus lacinia nisl, in suscipit orci mi et dolor.    ",
-    thumbnail:
-      "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
-  },
-  {
-    title: "Moonbeam",
-    content: "content is here",
-    description: "descrirption is here",
-    thumbnail:
-      "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
-  },
-  {
-    title: "Moonbeam",
-    content: "content is here",
-    description: "descrirption is here",
-    thumbnail:
-      "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
-  },
-  {
-    title: "Moonbeam",
-    content: "content is here",
-    description: "descrirption is here",
-    thumbnail:
-      "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
-  },
-  {
-    title: "Moonbeam",
-    content: "content is here",
-    description: "descrirption is here",
-    thumbnail:
-      "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
-  },
-  {
-    title: "Moonbeam",
-    content: "content is here",
-    description: "descrirption is here",
-    thumbnail:
-      "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
-  },
-  {
-    title: "Moonbeam",
-    content: "content is here",
-    description: "descrirption is here",
-    thumbnail:
-      "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
-  },
-  {
-    title: "Moonbeam",
-    content: "content is here",
-    description: "descrirption is here",
-    thumbnail:
-      "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1709569063/CSI%20Website/Events/hw7nppsn4j9l8qfvcstw.jpg",
-  },
-  
-];
+
