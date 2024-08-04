980f6e8af31a2426de697be473023facadef91ab
Kumar Harsh <kharsh560@gmail.com>
2024-08-04 12:41:00 +0530

    fixed the prod. bug by vercel.
    fixed the prod. bug by vercel, which came after raising PR for completed team and gallery pages.

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -11,7 +11,7 @@ function Gallery() {
   return (
     <div>
       <div className="sm:h-auto h-aut z-50 w-full dark:bg-grid-white/[0.2] bg-grid-black/[0.2]  flex py-2 justify-center">
-        <div className="text-3xl sm:text-7xl w-8/12  ">
+        <div className="text-3xl sm:text-7xl w-8/12">
           <div className="font-bold relative z-20 bg-clip-text pb-10 text-transparent text-center bg-gradient-to-r from-blue-600 to-indigo-200 ">
             Our Memories
           </div>
@@ -20,6 +20,21 @@ function Gallery() {
               className="sm:h-[40rem] h-[20rem] rounded-2xl"
               images={imagesOne}
             >
+              {/* Children -> Bug in production mode! */}
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  y: -80,
+                }}
+                animate={{
+                  opacity: 1,
+                  y: 0,
+                }}
+                transition={{
+                  duration: 0.6,
+                }}
+                className="z-50 flex flex-col justify-center items-center"
+              ></motion.div>
             </ImagesSlider>
           </div>
           
