5289ed3c798e1adc766f7cc9b219c1ede138c58e
joyal-jij0 <joyal2405@gmail.com>
2024-08-05 23:37:27 +0530

    fix: Remove unnecessary hover styles from GlareCard component

--- src/components/ui/glare-card.tsx
+++ src/components/ui/glare-card.tsx
@@ -119,7 +119,7 @@ export const GlareCard = ({
                 }
             }}
         >
-            <div className="h-full grid will-change-transform origin-center transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] [transform:rotateY(var(--r-x))_rotateX(var(--r-y))] rounded-[var(--radius)]  hover:[--opacity:0.6] hover:[--duration:200ms] hover:[--easing:linear] hover:filter-none overflow-hidden">
+            <div className="h-full grid will-change-transform origin-center transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] [transform:rotateY(var(--r-x))_rotateX(var(--r-y))] rounded-[var(--radius)]  overflow-hidden">
                 <div className="w-full  h-full grid [grid-area:1/1] mix-blend-soft-light [clip-path:inset(0_0_0_0_round_var(--radius))]">
                     <div
                         className={cn("h-full w-full", className)}
