0b63814b9254889a2c2c88095e613dc4f4800c53
joyal-jij0 <joyal2405@gmail.com>
2024-08-21 21:52:09 +0530

    feat: healthcheck route and connetDB function

---
+++ src/app/api/healthcheck/route.ts
@@ -0,0 +1,24 @@
+import dbConnect from "@/lib/dbConnect";
+
+export async function GET(request: Request){
+    try {
+        await dbConnect()
+
+        return Response.json({
+            success: true,
+            messsage: "All good man"
+        },
+        {status: 201}
+)
+    } catch (error) {
+        return Response.json(
+            {
+                success: false,
+                message: "Something went wrong",
+                data: error
+            },
+            {status: 500}
+        )
+    }
+    
+}
