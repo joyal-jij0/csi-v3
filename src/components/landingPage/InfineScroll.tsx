2252bd0629595752e6c23ade6db961179a90de55
Kshitij Dahiya <coderkd26@gmail.com>
2025-08-28 09:24:05 +0530

    Add 14 additional company logos to trusted by section - Added logos for Krafton India, IEEE Smart Cities, Microsoft Azure, and 11 other companies - Total logos now: 24 companies - Maintains original component structure and format (#70)

--- src/components/landingPage/InfineScroll.tsx
+++ src/components/landingPage/InfineScroll.tsx
@@ -12,7 +12,21 @@ const InfiniteScrollBrand = () => {
     { src: "https://res.cloudinary.com/dzvdh7yez/image/upload/v1722238968/meta_space_xhg8di.png", alt: "Meta Space" },
     { src: "https://res.cloudinary.com/dzvdh7yez/image/upload/v1722238965/momentum_ias_ptmdje.png", alt: "Momentum IAS" },
     { src: "https://res.cloudinary.com/dzvdh7yez/image/upload/v1722238948/programming_pathshala_new_cpoipf.jpg", alt: "Programming Pathshala" },
-    { src:"https://res.cloudinary.com/djhilzrxt/image/upload/v1722794668/Zenzia_tfxldh.jpg",alt:"zenzia"}
+    { src:"https://res.cloudinary.com/djhilzrxt/image/upload/v1722794668/Zenzia_tfxldh.jpg",alt:"zenzia"},
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317178/social-media-default-screen_afwm4g.jpg", alt: "Krafton India" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317178/YTCJwDgo_400x400_gib8gq.jpg", alt: "IEEE Smart Cities" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317178/wtn_logo_8_qh7tvv.png", alt: "kyndryl" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317178/the_belgian_waffle_co_logo_gnp6zd.jpg", alt: "The Belgian Waffle Co" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317178/stockedgelogoimage28102022144458_ra0sny.png", alt: "Stockedge" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317177/epyc_logo_dzluh8.jpg", alt: "EPYC" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317177/Mother_Dairy.svg_j84cj5.png", alt: "Mother Dairy" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317177/Social-Imagery-47_visg4h.webp", alt: "Unidays" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317177/CNCF_Community_Groups_Logo_2023_PRjuBUd_zvgrko.webp", alt: "Cloud Native Community groups" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317177/images_s3lguz.jpg", alt: "MapmyIndia" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756317176/360_F_238159412_HhVnn8jBWgYG9IoRSlPw2Iz5FMZz93CH_sly5np.jpg", alt: "Bistro" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756321734/reskilll_paqslv.png", alt: "Reskill" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756321570/Screenshot_2025-08-28_at_12.35.41_AM_eqnlbo.png", alt: "Launched Global" },
+    { src: "https://res.cloudinary.com/dcvl4olut/image/upload/v1756318894/Microsoft-Azure-Logo_vvmcjr.png", alt: "Microsoft Azure" },
     
   ];
 
